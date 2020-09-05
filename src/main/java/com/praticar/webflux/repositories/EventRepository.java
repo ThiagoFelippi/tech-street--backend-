bf4495689e75602a572007247983db943aee9686
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-05 20:10:26 -0300

    Event repository added

--- src/main/java/com/praticar/webflux/repositories/EventRepository.java
+++ src/main/java/com/praticar/webflux/repositories/EventRepository.java
@@ -10,6 +10,6 @@ import reactor.core.publisher.Mono;
 public interface EventRepository extends ReactiveCrudRepository<Event, Long> {
 
     Mono<Event> findById(long id);
-    Flux<Event> findByTag(String tag);
+    Flux<Event> findByTags(String tag);
     Mono<Event> findByName(String name);
 }
