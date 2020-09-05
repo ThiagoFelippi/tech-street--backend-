47227e4ab288b0ccf36f41cbcc109732874273db
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-05 17:47:41 -0300

    Change id to long type

--- src/main/java/com/praticar/webflux/entities/User.java
+++ src/main/java/com/praticar/webflux/entities/User.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 @Builder
 public class User implements Serializable {
     @Id
-    private Integer id;
+    private Long id;
 
     private String username;
     private String password;
