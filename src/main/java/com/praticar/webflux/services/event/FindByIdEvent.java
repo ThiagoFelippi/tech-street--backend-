35fe41282e593a0efc316acd8aee65f55f80ae4f
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-05 20:38:39 -0300

    Find by id (event) service fixed

--- src/main/java/com/praticar/webflux/services/event/FindByIdEvent.java
+++ src/main/java/com/praticar/webflux/services/event/FindByIdEvent.java
@@ -1,4 +1,18 @@
 package com.praticar.webflux.services.event;
 
+import com.praticar.webflux.entities.Event;
+import com.praticar.webflux.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
 public class FindByIdEvent {
+
+    private final EventRepository eventRepository;
+
+    public Mono<Event> execute(int id) {
+        return eventRepository.findById(id);
+    }
 }
