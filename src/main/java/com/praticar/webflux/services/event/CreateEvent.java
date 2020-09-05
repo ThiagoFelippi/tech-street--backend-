5c097d10c9999ffa31a1feaa7d5034e5a2e2084f
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-05 20:09:55 -0300

    CRUD operations for event

--- src/main/java/com/praticar/webflux/services/event/CreateEvent.java
+++ src/main/java/com/praticar/webflux/services/event/CreateEvent.java
@@ -1,4 +1,20 @@
 package com.praticar.webflux.services.event;
 
+import com.praticar.webflux.entities.Event;
+import com.praticar.webflux.entities.User;
+import com.praticar.webflux.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
 public class CreateEvent {
+    private final EventRepository eventRepository;
+    private final ValidateEvent validateEvent;
+
+    public Mono<Event> execute(Event event) throws Exception {
+        validateEvent.execute(event);
+        return eventRepository.save(event);
+    }
 }
