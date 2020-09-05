582853924102b3f9d969aede63c4f7052ec08a25
ThiagoFelippiNT <thiago.felippi@ntconsultcorp.com>
2020-09-05 17:36:31 -0300

    User controller added

---
+++ src/main/java/com/praticar/webflux/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.praticar.webflux.controllers;
+
+import com.praticar.webflux.entities.User;
+import com.praticar.webflux.services.user.CreateUser;
+import com.praticar.webflux.services.user.FindAllUsers;
+import com.praticar.webflux.services.user.FindByIdUser;
+import com.praticar.webflux.services.user.ValidateUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final FindAllUsers findAllUsers;
+    private final FindByIdUser findByIdUser;
+    private final CreateUser createUser;
+
+
+    @GetMapping()
+    public Flux<User> findAll(){
+        return findAllUsers.execute();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<User> findById(@PathVariable long id){
+        return findByIdUser.execute(id);
+    }
+
+    @PostMapping()
+    public Mono<User> createUser(@RequestBody User user) throws Exception {
+        return createUser.execute(user);
+    }
+
+}
