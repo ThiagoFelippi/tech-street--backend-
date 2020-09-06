93171dbb41f3167ffea4e82ccc99a780d3a19db6
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-06 11:40:22 -0300

    Hash password

--- src/main/java/com/praticar/webflux/services/user/CreateUser.java
+++ src/main/java/com/praticar/webflux/services/user/CreateUser.java
@@ -3,6 +3,7 @@ package com.praticar.webflux.services.user;
 import com.praticar.webflux.entities.User;
 import com.praticar.webflux.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -14,7 +15,16 @@ public class CreateUser {
 
     public Mono<User> execute(User user) throws Exception {
         User userValidated = validateUser.execute(user);
-        return userRepository.save(userValidated);
+
+        String passwordHashed = new BCryptPasswordEncoder().encode(userValidated.getPassword());
+        User userWithHashedPass = User
+                .builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(passwordHashed)
+                .build();
+
+        return userRepository.save(userWithHashedPass);
     }
 
 }
