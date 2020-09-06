f30684c50bb5312c674d9a03e677ff3ec65a562c
ThiagoFelippi <thiago.felippi@ntconsultcorp.com>
2020-09-06 13:12:35 -0300

    Dependendy to email added and implementation started

--- src/main/java/com/praticar/webflux/services/user/CreateUser.java
+++ src/main/java/com/praticar/webflux/services/user/CreateUser.java
@@ -3,6 +3,7 @@ package com.praticar.webflux.services.user;
 import com.praticar.webflux.entities.User;
 import com.praticar.webflux.entities.UserDTO;
 import com.praticar.webflux.repositories.UserRepository;
+import com.praticar.webflux.services.email.SendEmail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,13 +14,21 @@ public class CreateUser {
     private final UserRepository userRepository;
     private final ValidateUser validateUser;
     private final HashPassword hashPassword;
+    private final SendEmail sendEmail;
 
-    public Mono<User> execute(User user) throws Exception {
+    public UserDTO execute(User user) throws Exception {
         User userValidated = validateUser.execute(user);
 
         User userWithPasswordHashed = hashPassword.execute(userValidated);
 
-        return userRepository.save(userWithPasswordHashed);
+        sendEmail.sendSimpleMessage(
+                user.getUsername()+"@gmail.com",
+                "Seja bem vindo " + user.getUsername(),
+                "Parabéns, agora você faz parte do nosso banco de dados!"
+        );
+
+        Mono<User> userCreated = userRepository.save(userWithPasswordHashed);
+        return new UserDTO(userCreated);
     }
 
 }
